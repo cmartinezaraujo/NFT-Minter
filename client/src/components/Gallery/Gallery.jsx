1ca146b4fa6fed28858c75df78deb4f639874364
Cesar Martinez <cmartinezaraujo@ewu.edu>
2022-06-06 19:47:41 -0700

    Padding and text tweak

--- client/src/components/Gallery/Gallery.jsx
+++ client/src/components/Gallery/Gallery.jsx
@@ -8,7 +8,7 @@ export const Gallery = () => {
     const NFT_Example = {
         title: 'AI',
         owner: 'AiLab',
-        info: 'This is a  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi asperiores in suscipit quia, culpa optio, veniam ducimus iusto commodi eveniet ut quaerat harum. that is created by the NFT contract'
+        info: 'This is a Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi asperiores in suscipit quia, culpa optio, veniam ducimus iusto commodi elit.'
     };
 
     const {transactions} = useContext(NFTContext);
@@ -16,13 +16,13 @@ export const Gallery = () => {
     <section className='gallery'>
         <h1 className="heading" >Gallery</h1>
         <div className='nft-tokens'>
-        {/* {[...Array(10)].map((x, i) =>
+        {[...Array(10)].map((x, i) =>
             <NFT className="nft-card" key={i} img={nftImage} title={NFT_Example.title} owner={NFT_Example.owner} info={NFT_Example.info}/>
-        )} */}
+        )}
         {transactions.map((token, i) =>{
             //const img = 
             console.log("consumable", token);
-            return <NFT className="nft-card" key={i} img={`https://ipfs.io/ipfs/${token.image}`} title={token.name} owner={token.owner} info={token.description}/>
+            return <NFT className="nft-card" key={i} img={`https://ipfs.io/ipfs/${token.image}`} title={token.name} owner={token.owner} info={token.message}/>
         })}
         </div>
     </section>
