bacf38cce8026da16ae4ae602db4b21402da458d
Cesar Martinez <cmartinezaraujo@ewu.edu>
2022-06-07 14:04:57 -0700

    Implemented Loading component

---
+++ client/src/components/Loading/Loading.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./loading.css"
+
+
+export const Loading = () => {
+  return (
+    <div className="load-wrapp">
+    <div className="load-10">
+      <div class="bar"></div>
+      <p>Loading</p>
+    </div>
+  </div>
+  )
+}
