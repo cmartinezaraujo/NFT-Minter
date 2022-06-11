5fd592da7015eebb85511b2d6453b4f605faf7b8
Cesar Martinez <cmartinezaraujo@ewu.edu>
2022-06-10 19:32:59 -0700

    Redesigned create section -mobile

--- client/src/components/Create/Create.jsx
+++ client/src/components/Create/Create.jsx
@@ -3,6 +3,7 @@ import './create.css'
 import { NFTContext } from '../../Context/NFTContext'
 import { NFT } from '../NFT/NFT'
 import {Loading} from '../Loading/Loading'
+import gradient from '../../../assets/gradient.jpg'
 
 export const Create = () => {
 
@@ -27,47 +28,49 @@ export const Create = () => {
 
     //funtion to check character limit in string
   return (
-    <section className='create'>
+    <section className='create app__container'>
 
       <div className='NFT-container'>
         <NFT 
           owner = {formData.owner} 
           info={formData.message}  
           img ={
-            (formData.file != '') ? URL.createObjectURL(formData.file) : 'https://www.placecage.com/200/200'
+            (formData.file != '') ? URL.createObjectURL(formData.file) : '../../../assets/isometric-nft.jpg'
           } 
           title={formData.name}/>
       </div>
 
-      {isLoading ? (<Loading />) : (
+      {isLoading ? (<Loading />) : (!currentAccount) ? 
+      (
+        <div className='connect-prompt'>
+          <h3>Connect your Wallet</h3>
+          <p>Connect your Etherum wallet useing MetaMask. When you are ready to mint
+            your NFT you will be prompted by MetaMask to approave the transaction.
+            A minimum of .1 ETH is required to mint your NFT.
+          </p>
+          <button 
+              type='button' 
+              className='button-send'
+              onClick={connectWallet}
+              >Connect Wallet</button>
+        </div>
+      )
+      :(
       <div className='form-container'>
         <form className='create-form'>
-            <input className='input-field' placeholder='Address' name="address" type="text" onChange={(e) => handleChange(e, 'address')}/>
-            <input className='input-field' placeholder='Amount (ETH)' name="ammount" type="number" onChange={(e) => handleChange(e, 'ammount')}/>
-            <input className='input-field' placeholder='NFT name' name="name" type="text" onChange={(e) => handleChange(e, 'name')}/>
-            <input className='input-field' placeholder='Owner' name="owner" type="text" onChange={(e) => handleChange(e, 'owner')}/>
-            <input className='input-field' placeholder='Message' name="message" type="text" onChange={(e) => handleChange(e, 'message')}/>
-            <input className='input-brows' type="file" name='file' onChange={(e) => handleChange(e, 'file')}/>
+            <input className='input-field backdrop-blur' placeholder='Address' name="address" type="text" onChange={(e) => handleChange(e, 'address')}/>
+            <input className='input-field backdrop-blur' placeholder='Amount (ETH)' name="ammount" type="number" onChange={(e) => handleChange(e, 'ammount')}/>
+            <input className='input-field backdrop-blur' placeholder='NFT name' name="name" type="text" onChange={(e) => handleChange(e, 'name')}/>
+            <input className='input-field backdrop-blur' placeholder='Owner' name="owner" type="text" onChange={(e) => handleChange(e, 'owner')}/>
+            <input className='input-field backdrop-blur' placeholder='Message' name="message" type="text" onChange={(e) => handleChange(e, 'message')}/>
+            <input className='input-browse backdrop-blur' type="file" name='file' onChange={(e) => handleChange(e, 'file')}/>
+          
 
-           
-
-            {!currentAccount &&(
-            <button type='button' onClick={connectWallet} >
-              <p>
-                Connect Wallet
-              </p>
-            </button>
-          )}
-
-            {!currentAccount ? 
-            (<button 
+          <button 
               type='button' 
-              className='button-disabled'
-              >Send Now</button>) : 
-            (<button 
-              type='button' 
+              className='button-send'
               onClick={handleSubmit}
-              >Send Now</button>)}
+              >Mint NFT</button>
          </form>         
       </div>
       )}
