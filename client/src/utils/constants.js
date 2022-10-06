35a740e5fed1be817df02f5f0dbd587a95746308
Cesar Martinez Araujo <cesarmartinezaraujo@outlook.com>
2022-10-05 21:15:04 -0700

    Migrate contract to Goerli TestNet
    -Change contract address

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from './CMGalley.json';
 
 export const contractABI = abi.abi;
-export const contractAddress = '0xd5f3ede0DFB8eD5979606710e2e90f69850830E4';
+export const contractAddress = '0x5D6fcc328812e2b19afa0DefD487f186F7b5Ce47';
